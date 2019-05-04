60edcb53710262c320ba3a12b8031c9a288791d8
uripierreburmester <30432869+uripierreburmester@users.noreply.github.com>
2019-05-04 16:42:15 +1000

    Added detectTheGround.m - has yet to be refined

--- Code/detection.m
+++ Code/detection.m
@@ -2,56 +2,55 @@ clearvars();
 close all;
 
 v = VideoReader('Angry Birds In-game Trailer.avi');
+time = 56;
+v.CurrentTime = time;
 
 %% Read Frames
 % v.CurrentTime = 15; %Red bird and pigs
 % v.currentTime = 26; %Blue bird
 % v.currentTime = 31; %Yellow bird
 % v.currentTime = 39; %Black bird and pigs
-%  v.currentTime = 46; %White bird
+% v.currentTime = 46; %White bird
 
 theFrame = readFrame(v);
 
 %% Detect Birds
+% 
+% %%%%% -------- Red Bird ---------- %%%%%
+% redBirds = detectRedBird(theFrame);
+% 
+% %%%%% -------- Blue Bird ---------- %%%%%
+% bluBirds = detectBlueBird(theFrame);
+% 
+% %%%%% -------- Yellow Bird ---------- %%%%%
+% yelBirds = detectYellowBird(theFrame);
+% 
+% %%%%% -------- Black Bird ---------- %%%%%
+% blkBirds = detectBlackBird(theFrame);
+% 
+% %%%%% -------- White Bird ---------- %%%%%
+% whtBirds = detectWhiteBird(theFrame);
+% 
+% %%%%% -------- Green Pigs ---------- %%%%%
+% grenPigs = detectGreenPigs(theFrame);
 
-%%%%% -------- Red Bird ---------- %%%%%
-redBirds = detectRedBird(theFrame);
-
-%%%%% -------- Blue Bird ---------- %%%%%
-bluBirds = detectBlueBird(theFrame);
-
-%%%%% -------- Yellow Bird ---------- %%%%%
-yelBirds = detectYellowBird(theFrame);
-
-%%%%% -------- Black Bird ---------- %%%%%
-blkBirds = detectBlackBird(theFrame);
-
-%%%%% -------- White Bird ---------- %%%%%
-whtBirds = detectWhiteBird(theFrame);
-
-%%%%% -------- Green Pigs ---------- %%%%%
-grenPigs = detectGreenPigs(theFrame);
+%%%%% -------- The Ground ---------- %%%%%
+tic;
+gndVect = detectTheGround(theFrame, time);
+t = toc;
 
 %% Display
 
-v.CurrentTime = 1;
-
-while v.hasFrame
     
-    figure();
-    imshow(theFrame); hold on;
-    
-    DrawRectangles(redBirds, 'red');
-    DrawRectangles(bluBirds, 'blue');
-    DrawRectangles(yelBirds, 'yellow');
-    DrawRectangles(blkBirds, 'black');
-    DrawRectangles(whtBirds, 'white');
-    DrawRectangles(grenPigs, 'green');
-   
-    
-    v.CurrentTime = v.CurrentTime + 1;
-    
-end
+% figure();
+% imshow(theFrame); hold on;
+% 
+% DrawRectangles(redBirds, 'red');
+% DrawRectangles(bluBirds, 'blue');
+% DrawRectangles(yelBirds, 'yellow');
+% DrawRectangles(blkBirds, 'black');
+% DrawRectangles(whtBirds, 'white');
+% DrawRectangles(grenPigs, 'green');
 
 
 function [] = DrawRectangles(birdsOrPigs, col)
